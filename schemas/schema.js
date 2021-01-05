04569aa95583c272c9cf5975dbedc83aca4ee3f9
David Ballester Mena <david.ballester.mena@gmail.com>
2021-01-05 22:04:16 +0100

    chore: prettier!

--- schemas/schema.js
+++ schemas/schema.js
@@ -15,22 +15,22 @@ import plotSummaries from './plotSummaries'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
-  name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
-    movie,
-    person,
-    screening,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
-    plotSummary,
-    plotSummaries,
-    castMember,
-    crewMember,
-  ]),
+    // We name our schema
+    name: 'default',
+    // Then proceed to concatenate our document type
+    // to the ones provided by any plugins that are installed
+    types: schemaTypes.concat([
+        // The following are document types which will appear
+        // in the studio.
+        movie,
+        person,
+        screening,
+        // When added to this list, object types can be used as
+        // { type: 'typename' } in other document schemas
+        blockContent,
+        plotSummary,
+        plotSummaries,
+        castMember,
+        crewMember,
+    ]),
 })
